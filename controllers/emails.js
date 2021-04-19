7f843b230fa50b3290339ccb93e9ca6a6b5af17b
huongnt.iteal@gmail.com <huongnt.iteal@gmail.com>
2021-04-19 18:08:43 +0700

    test email

--- controllers/emails.js
+++ controllers/emails.js
@@ -1,23 +1,35 @@
 const Email = require("../models/email");
-const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const nodemailer = require("nodemailer");
+
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.sendEmail = (req, res, next) => {
-  const msg = {
-    to: req.body.to,
-    from: req.body.from, // Use the email address or domain you verified above
-    subject: req.body.subject,
-    text: req.body.text,
-    html: req.body.html,
-  };
-  console.log('msg: ' + msg.from);
-  sgMail
-  .send(msg)
-  .then(() => {
-    console.log('emails sent successfully!');
-  }).catch(error => {
-    console.log(error);
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.googlemail.com', // Gmail Host
+    port: 465, // Port
+    secure: true, // this is true as port is 465
+    auth: {
+      user: 'tripcheap.pay@gmail.com', // generated ethereal user
+      pass: 'Huong_18021999', // generated ethereal password
+    },
   });
+
+  var mailOptions = {
+    from: '"Tripcheap" <tripcheap.pay@gmail.com>',
+    to: req.body.to,
+    subject: req.body.subject,
+    text: req.body.html
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
+  console.log("Message sent: %s", info.messageId);
   
 }
 
